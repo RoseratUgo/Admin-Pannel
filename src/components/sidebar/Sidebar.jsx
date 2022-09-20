fdbb597dd14cc312b97462804d244720689b6aa3
ROSERAT Ugo <roserat.ugo@gmail.com>
2022-09-20 16:15:17 +0200

    Navbar and sidebar
    The navbar and sidebar was finsh for the design

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,86 @@
-import "../../../../panneladmin/src/components/sidebar/sidebar.scss"
+import "../../../../panneladmin/src/components/sidebar/sidebar.scss";
+
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import AssessmentRoundedIcon from '@mui/icons-material/AssessmentRounded';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
+import SettingsApplicationsRoundedIcon from '@mui/icons-material/SettingsApplicationsRounded';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = () => {
   return (
-    <div>
-      Sidebar
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">AC-Concepts</span>
+      </div>
+      <hr/>
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className="icon"/>
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PersonOutlineOutlinedIcon className="icon"/>
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreIcon className="icon"/>
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className="icon"/>
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className="icon"/>
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <AssessmentRoundedIcon className="icon"/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneOutlinedIcon className="icon"/>
+            <span>Notification</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon"/>
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon"/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsRoundedIcon className="icon"/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountCircleOutlinedIcon className="icon"/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutIcon className="icon"/>
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,17 @@
 import "../../../../panneladmin/src/pages/home/home.scss"
+import Navbar from "../../components/navbar/Navbar"
 import Sidebar from "../../components/sidebar/Sidebar"
 
 const Home = () => {
   return (
     <div className="home">
         <Sidebar/>
+        <div className="homeContainer">
+          <Navbar/>
+          <div className="widgets">
+            
+          </div>
+        </div>
     </div>
   )
 }
